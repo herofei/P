7ab098549ab95159e07fa274478fc33bac93c653
Mat He <mat.he@qq.com>
2018-06-06 00:01:34 +0800

    部分编码

--- src/p.js
+++ src/p.js
@@ -10,7 +10,7 @@
     if (typeof exports === 'object' && typeof module === 'object') {
         module.exports = definition();
 
-    // RequireJS
+        // RequireJS
     } else if (typeof define === 'function' && define.amd) {
         define(definition);
 
@@ -26,13 +26,13 @@
     } else {
         throw new Error('This environment was not anticipated by p.');
     }
-})(function() {
+})(function () {
     var PENDING = 0,
         FULFILLED = 1,
         REJECTED = 2;
-        util = {},
+    util = {},
         typeArr = ['Function', 'Object', 'Array'];
-    
+
     (function () {
         typeArr.foreach(function (item) {
             util['is' + item] = function (type) {
@@ -41,7 +41,7 @@
         })
     })();
 
-    function Promise (resolver) {
+    function Promise(resolver) {
         if (!util.isFunction(resolver)) {
             throw new TypeError('The arguments must be a function!');
         }
@@ -51,7 +51,7 @@
         this.onFulfilledCallbacks = [];
         this.onRejectedCallbacks = [];
 
-        function resolve (value) {
+        function resolve(value) {
             if (me.status !== PENDING) {
                 return;
             }
@@ -60,7 +60,7 @@
                 return value.then(resolve, reject);
             }
 
-            setTimeout (function () {
+            setTimeout(function () {
                 // 异步执行所有回调
                 me.status = FULFILLED;
                 me.value = value;
@@ -70,12 +70,12 @@
             });
         }
 
-        function reject (reason) {
+        function reject(reason) {
             if (me.status !== PENDING) {
                 return;
             }
-            
-            setTimeout (function () {
+
+            setTimeout(function () {
                 // 异步执行所有回调
                 me.status = REJECTED;
                 me.value = reason;
@@ -93,15 +93,70 @@
     }
 
     Promise.prototype.then = function (onFulfilled, onRejected) {
+        var me = this,
+            promise,
+            resolver;
+        
+        onFulfilled = util.isFunction(onFulfilled) ? onFulfilled : function (value) {
+            return value;
+        }
+        onRejected = util.isFunction(onRejected) ? onRejected : function (reason) {
+            throw reason;
+        }
 
+        if (me.status === PENDING) {
+            return promise = new Promise(function (resolve, reject) {
+                me.onFulfilledCallbacks.push(function (value) {
+                    try {
+                        var value = onFulfilled(value);
+                        resolvePromise(promise, value, resolve, reject);
+                    } catch (reason) {
+                        reject(reason);
+                    }
+                })
+
+                me.onRejectedCallbacks.push(function (reason) {
+                    try {
+                        var reason = onRejected(reason);
+                        resolvePromise(promise, reason, resolve, reject);
+                    } catch (reason) {
+                        reject(reason);
+                    }
+                })
+            })
+        }
+
+        if (me.status === FULFILLED) {
+            resolver = onFulfilled;
+        }
+
+        if (me.status === REJECTED) {
+            resolver = onRejected;
+        }
+
+        return createPromise(resolver, me.value);
+    }
+
+    function createPromise (resolver, value) {
+        var promise;
+        return promise = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                try {
+                    value = resolver(value);
+                    resolvePromise(promise, value, resolve, reject);
+                } catch (reason) {
+                    reject(reason);
+                }
+            });
+        })
     }
 
     Promise.prototype.catch = function (onRejected) {
-
+        return this.then(null, onRejected);
     }
 
     Promise.all = function () {
-
+        
     }
 
     Promise.race = function () {
